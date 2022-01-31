400287da86733dff2498fead222c918ddbdf1516
Harshal Sheth <hsheth2@gmail.com>
2022-01-31 11:49:12 -0500

    upload cava 0.7.5

--- ppa.sh
+++ ppa.sh
@@ -4,7 +4,7 @@
 set -e
 
 # Version of the cava submodule, plug an extra tag.
-VERSION=0.7.3
+VERSION=0.7.5
 DEBIAN_REVISION=1  # this may not contain a dash
 
 # The distro we want to build a package for.
